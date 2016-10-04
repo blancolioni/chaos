b94e5fe6afa50c2933bd9c70858bd4f518cebf98
Fraser Wilson <blancolioni@gmail.com>
2016-10-04 17:47:03 +0200

    Chaos.Powers.Attacks: added damage calculation

--- src/powers/chaos-powers-attacks.adb
+++ src/powers/chaos-powers-attacks.adb
@@ -4,10 +4,94 @@ with Chaos.Localisation;
 
 with Chaos.Expressions.Environments;
 with Chaos.Expressions.Numbers;
+with Chaos.Expressions.Vectors;
 
 with Chaos.UI;
 
 package body Chaos.Powers.Attacks is
+
+   procedure Apply_Power_Effects
+     (Actor       : Chaos.Actors.Chaos_Actor;
+      Defender    : Chaos.Actors.Chaos_Actor;
+      Power       : Chaos.Powers.Chaos_Power;
+      Environment : Chaos.Expressions.Chaos_Environment;
+      Effects     : Power_Effect_Vectors.Vector;
+      Hit         : Boolean;
+      Critical    : Boolean);
+
+   -------------------------
+   -- Apply_Power_Effects --
+   -------------------------
+
+   procedure Apply_Power_Effects
+     (Actor       : Chaos.Actors.Chaos_Actor;
+      Defender    : Chaos.Actors.Chaos_Actor;
+      Power       : Chaos.Powers.Chaos_Power;
+      Environment : Chaos.Expressions.Chaos_Environment;
+      Effects     : Power_Effect_Vectors.Vector;
+      Hit         : Boolean;
+      Critical    : Boolean)
+   is
+      pragma Unreferenced (Hit);
+
+      use Chaos.Expressions;
+      use Chaos.Expressions.Numbers;
+      use Chaos.Expressions.Vectors;
+      use Chaos.Localisation;
+
+      Hit_Environment : Chaos_Environment :=
+                          New_Environment
+                            (Environment);
+      Normal_Damage   : Integer := 0;
+      Damage_Type     : Power_Damage_Type := Normal;
+   begin
+      if Critical then
+         Chaos.Expressions.Insert
+           (Hit_Environment, "maximum-roll",
+            Chaos.Expressions.Always);
+      end if;
+
+      if Power.Implement = Weapon then
+         Chaos.Expressions.Insert
+           (Hit_Environment, "weapon",
+            Chaos.Dice.To_Expression
+              (Actor.Creature.Active_Weapon.Damage));
+      end if;
+
+      for Effect of Effects loop
+         for I in 1 .. Length (Effect) loop
+            declare
+               E : constant Chaos_Expression :=
+                     Evaluate (Get (Effect, I), Hit_Environment);
+               D : Integer;
+            begin
+               if Is_Power_Damage_Type (E) then
+                  Damage_Type := Get_Power_Damage_Type (E);
+               else
+                  D := Integer'Max (To_Integer (E), 1);
+                  if Damage_Type /= Normal then
+                     Chaos.UI.Current_UI.Display_Text
+                       (Local_Text
+                          ("takes-damage-type",
+                           Defender.Short_Name, D'Img,
+                           Local_Text
+                             (Power_Damage_Type'Image
+                                  (Damage_Type))));
+                  else
+                     Normal_Damage := Normal_Damage + D;
+                  end if;
+               end if;
+            end;
+         end loop;
+      end loop;
+
+      if Normal_Damage > 0 then
+         Chaos.UI.Current_UI.Display_Text
+           (Local_Text
+              ("takes-damage",
+               Defender.Short_Name, Normal_Damage'Img));
+      end if;
+   end Apply_Power_Effects;
 
    ------------
    -- Attack --
@@ -18,6 +102,8 @@ package body Chaos.Powers.Attacks is
       Defender  : Chaos.Actors.Chaos_Actor;
       Power     : Chaos.Powers.Chaos_Power)
    is
+      use Chaos.Expressions;
+
       Environment : Chaos.Expressions.Chaos_Environment :=
                       Chaos.Expressions.Prepend_Environment
                         (Actor.Local_Environment,
@@ -77,6 +163,12 @@ package body Chaos.Powers.Attacks is
                elsif Hit
                then "attack-hit"
                else "attack-miss")));
+
+         Apply_Power_Effects
+           (Actor, Defender, Power, Environment,
+            (if Hit then Power.Hit else Power.Miss),
+            Hit, Critical);
+
       end;
 
    end Attack;
