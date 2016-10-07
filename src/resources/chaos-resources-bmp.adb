34e1f6ce9c821bed13c07012d6895cf1ede61694
Fraser Wilson <blancolioni@gmail.com>
2016-10-07 06:52:43 +0200

    Chaos.Resources.Bmp body

---
+++ src/resources/chaos-resources-bmp.adb
@@ -0,0 +1,71 @@
+package body Chaos.Resources.Bmp is
+
+   -----------
+   -- Color --
+   -----------
+
+   function Color
+     (Resource : Bmp_Resource'Class;
+      X, Y     : Natural)
+      return Resource_Color
+   is
+      Result : constant WL.Bitmap_IO.Colour_Type :=
+                 WL.Bitmap_IO.Colour (Resource.BM, X, Y);
+   begin
+      return (WL.Binary_IO.Word_8 (Result.R),
+              WL.Binary_IO.Word_8 (Result.G),
+              WL.Binary_IO.Word_8 (Result.B),
+              WL.Binary_IO.Word_8 (Result.Alpha));
+
+   end Color;
+
+   -----------------
+   -- Color_Index --
+   -----------------
+
+   function Color_Index
+     (Resource : Bmp_Resource'Class;
+      X, Y     : Natural)
+      return Natural
+   is
+   begin
+      return Natural (WL.Bitmap_IO.Colour_Index (Resource.BM, X, Y));
+   end Color_Index;
+
+   ------------
+   -- Height --
+   ------------
+
+   function Height
+     (Resource : Bmp_Resource'Class)
+      return Natural
+   is
+   begin
+      return WL.Bitmap_IO.Height (Resource.BM);
+   end Height;
+
+   ----------
+   -- Load --
+   ----------
+
+   overriding procedure Load
+     (Resource : in out Bmp_Resource)
+   is
+   begin
+      WL.Binary_IO.Set_Offset (Resource.File, 0);
+      WL.Bitmap_IO.Read (Resource.BM, Resource.File);
+   end Load;
+
+   -----------
+   -- Width --
+   -----------
+
+   function Width
+     (Resource : Bmp_Resource'Class)
+      return Natural
+   is
+   begin
+      return WL.Bitmap_IO.Width (Resource.BM);
+   end Width;
+
+end Chaos.Resources.Bmp;
