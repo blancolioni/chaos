806298a8a603cb604187fa0d8ea056b59f9d0d90
Fraser Wilson <blancolioni@gmail.com>
2016-10-18 17:16:20 +0200

    Implemented action GiveItem

--- src/creatures/chaos-creatures-primitives.adb
+++ src/creatures/chaos-creatures-primitives.adb
@@ -6,6 +6,10 @@ package body Chaos.Creatures.Primitives is
      (Store : in out Lith.Objects.Object_Store'Class)
       return Lith.Objects.Object;
 
+   function Evaluate_Chaos_Give_Item
+     (Store : in out Lith.Objects.Object_Store'Class)
+      return Lith.Objects.Object;
+
    --------------------
    -- Add_Primitives --
    --------------------
@@ -15,6 +19,8 @@ package body Chaos.Creatures.Primitives is
    begin
       Define_Function ("chaos-get-reaction", 2,
                        Evaluate_Chaos_Get_Reaction'Access);
+      Define_Function ("chaos-give-item", 3,
+                       Evaluate_Chaos_Give_Item'Access);
    end Add_Primitives;
 
    ---------------------------------
@@ -31,5 +37,58 @@ package body Chaos.Creatures.Primitives is
          .Get_Reaction
            (Chaos_Creature (Chaos.Objects.To_Object (Store.Argument (2)))));
    end Evaluate_Chaos_Get_Reaction;
+
+   ------------------------------
+   -- Evaluate_Chaos_Give_Item --
+   ------------------------------
+
+   function Evaluate_Chaos_Give_Item
+     (Store : in out Lith.Objects.Object_Store'Class)
+      return Lith.Objects.Object
+   is
+      Giver : constant Chaos_Creature :=
+                Chaos_Creature (Chaos.Objects.To_Object (Store.Argument (1)));
+      Entity : constant Chaos.Entities.Chaos_Entity :=
+                 Chaos.Entities.Chaos_Entity
+                   (Chaos.Objects.To_Object (Store.Argument (2)));
+      Item   : constant Chaos.Items.Chaos_Item :=
+                 Giver.Item (Entity);
+      Receiver : constant Chaos_Creature :=
+                   Chaos_Creature
+                     (Chaos.Objects.To_Object (Store.Argument (3)));
+
+      procedure Update_Giver
+        (Creature : in out Chaos_Creature_Record'Class);
+
+      procedure Update_Receiver
+        (Creature : in out Chaos_Creature_Record'Class);
+
+      ------------------
+      -- Update_Giver --
+      ------------------
+
+      procedure Update_Giver
+        (Creature : in out Chaos_Creature_Record'Class)
+      is
+      begin
+         Creature.Remove_Entity (Entity);
+      end Update_Giver;
+
+      ---------------------
+      -- Update_Receiver --
+      ---------------------
+
+      procedure Update_Receiver
+        (Creature : in out Chaos_Creature_Record'Class)
+      is
+      begin
+         Creature.Add_Item (Item);
+      end Update_Receiver;
+
+   begin
+      Giver.Update (Update_Giver'Access);
+      Receiver.Update (Update_Receiver'Access);
+      return Item.To_Expression;
+   end Evaluate_Chaos_Give_Item;
 
 end Chaos.Creatures.Primitives;
