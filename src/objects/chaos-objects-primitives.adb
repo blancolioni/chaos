d09551b8f5febe15da11f487f322a31f226a7dea
Fraser Wilson <blancolioni@gmail.com>
2016-11-01 13:40:15 +0100

    Chaos.Objects.Primitives: added chaos-set-flag and chaos-clear-flag

--- src/objects/chaos-objects-primitives.adb
+++ src/objects/chaos-objects-primitives.adb
@@ -10,6 +10,10 @@ with Chaos.Logging;
 
 package body Chaos.Objects.Primitives is
 
+   function Evaluate_Chaos_Clear_Flag
+     (Store : in out Lith.Objects.Object_Store'Class)
+      return Lith.Objects.Object;
+
    function Evaluate_Chaos_Flag
      (Store : in out Lith.Objects.Object_Store'Class)
       return Lith.Objects.Object;
@@ -22,10 +26,18 @@ package body Chaos.Objects.Primitives is
      (Store : in out Lith.Objects.Object_Store'Class)
       return Lith.Objects.Object;
 
+   function Evaluate_Chaos_Set_Flag
+     (Store : in out Lith.Objects.Object_Store'Class)
+      return Lith.Objects.Object;
+
    function Evaluate_Chaos_Set_Trigger
      (Store : in out Lith.Objects.Object_Store'Class)
       return Lith.Objects.Object
    is (Store.Argument (1));
+
+   procedure Set_Flag_Value
+     (Store : in out Lith.Objects.Object_Store'Class;
+      Value : Boolean);
 
    --------------------
    -- Add_Primitives --
@@ -36,12 +48,29 @@ package body Chaos.Objects.Primitives is
       Lith.Objects.Interfaces.Define_Function
         ("chaos-flag", 2, Evaluate_Chaos_Flag'Access);
       Lith.Objects.Interfaces.Define_Function
+        ("chaos-set-flag", 2, Evaluate_Chaos_Set_Flag'Access);
+      Lith.Objects.Interfaces.Define_Function
+        ("chaos-clear-flag", 2, Evaluate_Chaos_Clear_Flag'Access);
+      Lith.Objects.Interfaces.Define_Function
         ("chaos-has-item", 2, Evaluate_Chaos_Has_Item'Access);
       Lith.Objects.Interfaces.Define_Function
         ("chaos-object-with-code", 1, Evaluate_Chaos_Object_With_Code'Access);
       Lith.Objects.Interfaces.Define_Function
         ("chaos-set-trigger", 2, Evaluate_Chaos_Set_Trigger'Access);
    end Add_Primitives;
+
+   -------------------------------
+   -- Evaluate_Chaos_Clear_Flag --
+   -------------------------------
+
+   function Evaluate_Chaos_Clear_Flag
+     (Store : in out Lith.Objects.Object_Store'Class)
+      return Lith.Objects.Object
+   is
+   begin
+      Set_Flag_Value (Store, False);
+      return Store.Argument (1);
+   end Evaluate_Chaos_Clear_Flag;
 
    -------------------------
    -- Evaluate_Chaos_Flag --
@@ -127,5 +156,64 @@ package body Chaos.Objects.Primitives is
       end if;
       return Result.To_Expression;
    end Evaluate_Chaos_Object_With_Code;
+
+   -----------------------------
+   -- Evaluate_Chaos_Set_Flag --
+   -----------------------------
+
+   function Evaluate_Chaos_Set_Flag
+     (Store : in out Lith.Objects.Object_Store'Class)
+      return Lith.Objects.Object
+   is
+   begin
+      Set_Flag_Value (Store, True);
+      return Store.Argument (1);
+   end Evaluate_Chaos_Set_Flag;
+
+   --------------------
+   -- Set_Flag_Value --
+   --------------------
+
+   procedure Set_Flag_Value
+     (Store : in out Lith.Objects.Object_Store'Class;
+      Value : Boolean)
+   is
+      use Lith.Objects, Lith.Objects.Symbols;
+      This   : constant Object := Store.Argument (1);
+      Object : constant Chaos_Object :=
+                 (if Is_Object (This)
+                  then To_Object (This)
+                  elsif Is_Symbol (This)
+                  then Search.Find_Object
+                    (Get_Name (To_Symbol (This)))
+                  else raise Constraint_Error
+                    with "expected a code or object, but found "
+                  & Store.Show (This));
+      Flag   : constant String :=
+                 Lith.Objects.Symbols.Get_Name
+                   (Lith.Objects.To_Symbol (Store.Argument (2)));
+
+      procedure Update_Flag (Rec : in out Memor.Root_Record_Type'Class);
+
+      -----------------
+      -- Update_Flag --
+      -----------------
+
+      procedure Update_Flag (Rec : in out Memor.Root_Record_Type'Class) is
+      begin
+         if Value then
+            Root_Chaos_Object_Record'Class (Rec).Set_Flag (Flag);
+         else
+            Root_Chaos_Object_Record'Class (Rec).Clear_Flag (Flag);
+         end if;
+      end Update_Flag;
+
+   begin
+      if Object /= null then
+         Object.Object_Database.Update
+           (Object.Reference, Update_Flag'Access);
+
+      end if;
+   end Set_Flag_Value;
 
 end Chaos.Objects.Primitives;
