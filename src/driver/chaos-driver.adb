83cc5c573fa8818630aedcc931430acfd9cccfaf
Fraser Wilson <blancolioni@gmail.com>
2016-11-10 17:08:21 +0100

    Chaos.Driver: extra script round after Shank dialog to show that we enter combat

--- src/driver/chaos-driver.adb
+++ src/driver/chaos-driver.adb
@@ -169,6 +169,7 @@ begin
          Chaos.Game.Current_Game.Travel ("AR2602", "EXIT2600");
          Chaos.Game.Current_Game.Script_Round;
          Chaos.Game.Current_Game.Select_Option (1);
+         Chaos.Game.Current_Game.Script_Round;
 
          UI.Stop;
       end;
